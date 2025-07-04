dbcec283bf1074fe8ce55d43444b69ad9058b947
H_Ghurbani <52192905+HGhurbani@users.noreply.github.com>
2025-07-04 18:56:13 +0300

    feat: add firebase integration

---
+++ src/lib/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbW7bX1m10rPyLtqxdec6f8I7u09-Dcq0",
+  authDomain: "molhem-book-store.firebaseapp.com",
+  projectId: "molhem-book-store",
+  storageBucket: "molhem-book-store.firebasestorage.app",
+  messagingSenderId: "405854542171",
+  appId: "1:405854542171:web:f5ec90eca02e261da8a27e",
+  measurementId: "G-J7N2QML49Z"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export default app;
