9c5ba96169a8469f682f89b8ab9bcc45e15f17de
H_Ghurbani <52192905+HGhurbani@users.noreply.github.com>
2025-07-05 10:56:09 +0300

    fix: correct firebase storage bucket

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDbW7bX1m10rPyLtqxdec6f8I7u09-Dcq0",
   authDomain: "molhem-book-store.firebaseapp.com",
   projectId: "molhem-book-store",
-  storageBucket: "molhem-book-store.firebasestorage.app",
+  storageBucket: "molhem-book-store.appspot.com",
   messagingSenderId: "405854542171",
   appId: "1:405854542171:web:f5ec90eca02e261da8a27e",
   measurementId: "G-J7N2QML49Z"
